ee1f7b6ea1496d97fd41c2d75a80ca5639aee060
bakytdeveloper <bakytdeveloper@gmail.com>
2026-03-15 13:46:39 +0600

    Корректирую фаил компонент AboutEditor.jsx

--- client/src/components/Admin/AboutEditor/AboutEditor.jsx
+++ client/src/components/Admin/AboutEditor/AboutEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import './AboutEditor.css';
 import {
     FaPlus, FaTrash, FaEdit, FaTimes, FaArrowUp, FaArrowDown,
